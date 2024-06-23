f6e084d1b8540d7127cca295dfa9ec283cb41fcb
laregn <lays.lays99@outlook.com>
2024-06-23 19:39:49 -0300

    alterado numero limite

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 3');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
 exibirMensagemInicial();
